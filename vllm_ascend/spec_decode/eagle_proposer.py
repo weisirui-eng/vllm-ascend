8af634f17eb5371151ab439fa5f5f7276e8b9dbf
weisirui-eng <weisirui@h-partners.com>
2025-09-10 11:51:09 +0800

    resove list[Any] has no attribute cpu
    Signed-off-by: weisirui-eng <weisirui@h-partners.com>

--- vllm_ascend/spec_decode/eagle_proposer.py
+++ vllm_ascend/spec_decode/eagle_proposer.py
@@ -280,7 +280,8 @@ class EagleProposer(Proposer):
                 target_hidden_states = torch.cat(
                     [h[token_indices] for h in aux_hidden_states], dim=-1)
             else:
-                target_hidden_states = hidden_states[token_indices]
+                if isinstance(hidden_states, torch.Tensor):
+                    target_hidden_states = hidden_states[token_indices]
             target_slot_mapping = eagle_attn_metadata.slot_mapping[
                 token_indices]
 
